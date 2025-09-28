5bcd54f0e81438af34b3e8731d7cd55e04de5c6d
Arashad Dodhiya <arashadodhiya4849@gmail.com>
2025-09-28 10:21:54 +0530

    user model change added password field

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,24 @@
 // src/models/User.js
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, index: true },
-  // since mock auth accepts any credentials, we store minimal info
+  username: { type: String, required: true, unique: true, index: true },
+  password: { type: String, required: true }, // hashed password
   createdAt: { type: Date, default: Date.now }
 });
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Method to compare password
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
